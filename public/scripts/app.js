24c6d8aa2b25ebbc02f469cd53acbe41d7d8dcd7
Katie So <katieso86@gmail.com>
2019-10-22 15:39:22 -0700

    search input is geocoded and setCenter on Map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,115 +1,100 @@
 $(document).ready(function(){
+  var googleMap = $('.googleMap');
+  var searchInput
   var infowindow = null;
   var geocoder;
   var map;
+
+  googleMap.hide();
 
   $.ajax({
         method: 'GET',
         url: '/api/profileLocations',
         success: function(alltrucks){
-          initMap();
-          function initMap() {
-            map = new google.maps.Map(document.getElementById('googleMap'), {
-              zoom: 12,
-              center: {lat: 37.77, lng: -122.44}
-            });
-
-            setMarkers(map, alltrucks);
-
-            infowindow = new google.maps.InfoWindow({
-              content: "loading"
-            });
-          }
-
-          function setMarkers(map, trucks){
-            console.log("this is trucks: " + trucks)
-            for(i=0; i<trucks.profile.length; i++){
-              setMarker(map, trucks.profile[i])
-            }
-          }
-
-          function setMarker(map, trucks){
-
-            geocoder = new google.maps.Geocoder();
-            geocoder.geocode( { 'address': trucks.location }, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                map.setCenter(results[0].geometry.location);
-
-                var marker = new google.maps.Marker({
-                    position: results[0].geometry.location,
-                    map: map,
-                    html: '<div id="content">'+
-                            '<div id="siteNotice">'+
-                            '</div>'+
-                            '<h2>'+trucks.truckname +'</h2>'+
-                            '<div id="bodyContent">'+
-                            '<p>'+ trucks.foodtype +'</p>'+
-                            '</div>'+
-                            '</div>'
-                });
-
-                google.maps.event.addListener(marker, "click", function () {
-                    infowindow.setContent(this.html);
-                    infowindow.open(map, this);
-                });
-            }
-            else {
-                alert("Geocode was not successful for the following reason: " + status);
-                  }
-              });
-            }
-
-
-          // function geocodeAddress(geocoder, resultsMap) {
-          //   var locationHidden = 0
-          //   for(i=0; i<alltrucks.profile.length; i++){
-          //     // if(alltrucks.profile[i].location != undefined ){
-          //     // console.log("location: "+alltrucks.profile[i].location)
-          //       var address = alltrucks.profile[i].location
-          //       var business =  alltrucks.profile[i].truckname
-          //       var type = alltrucks.profile[i].foodtype
-          //       var contentString = '<div id="content">'+
-          //         '<div id="siteNotice">'+
-          //         '</div>'+
-          //         '<h1 id="firstHeading" class="firstHeading">'+ business +'</h1>'+
-          //         '<div id="bodyContent">'+
-          //         '<p>'+ type +'</p>'+
-          //         '</div>'+
-          //         '</div>';
-          //
-          //       geocoder.geocode({'address': address}, function(results, status) {
-          //         if (status === 'OK') {
-          //           console.log("results: " + JSON.stringify(results))
-          //           console.log("business is: " + business)
-          //           console.log("address is: "+ address)
-          //           console.log("type is: "+ type)
-          //
-          //           resultsMap.setCenter(results[0].geometry.location);
-          //           console.log("results[0].geometry.location: " + results[0].geometry.location)
-          //           var marker = new google.maps.Marker({
-          //             map: resultsMap,
-          //             position: results[0].geometry.location
-          //             // title: 'business'
-          //           });
-          //
-          //           marker.addListener('click', function() {
-          //             infowindow.open(resultsMap, marker);
-          //           });
-          //
-          //
-          //
-          //         } else {
-          //           locationHidden++
-          //           console.log(locationHidden + " foodtruck currently unactive")
-          //         }
-          //       });
-          //     // }
-          //   }
-          // }
-
-
+          initMap(alltrucks);
       }
     })
+
+
+    function initMap(alltrucks) {
+      map = new google.maps.Map(document.getElementById('googleMap'), {
+        zoom: 12,
+        center: {lat: 37.77, lng: -122.44}
+      });
+
+      setMarkers(map, alltrucks);
+      infowindow = new google.maps.InfoWindow({
+        content: "loading"
+      });
+    }
+
+    function setMarkers(map, trucks){
+      for(i=0; i<trucks.profile.length; i++){
+        setMarker(map, trucks.profile[i])
+      }
+    }
+
+    function setMarker(map, trucks){
+      geocoder = new google.maps.Geocoder();
+      geocoder.geocode( { 'address': trucks.location }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+          var marker = new google.maps.Marker({
+              position: results[0].geometry.location,
+              map: map,
+              html: '<div id="markerWindow">'+
+                      '<div id="siteNotice">'+
+                      '</div>'+
+                      '<h2>'+trucks.truckname +'</h2>'+
+                      '<div id="bodyContent">'+
+                      '<p>'+ trucks.foodtype +'</p>'+
+                      '</div>'+
+                      '</div>'
+          });
+          google.maps.event.addListener(marker, "click", function () {
+              infowindow.setContent(this.html);
+              infowindow.open(map, this);
+          });
+          marker.addListener('mouseover', function() {
+              console.log("see this?")
+          });
+
+      }
+      else {
+          alert("Geocode was not successful for the following reason: " + status);
+            }
+        });
+      }
+
+    $('#markerWindow').mouseover(function() {
+          console.log("can you detect this?")
+      });
+
+    $('.search-button').on('click', function(e){
+      e.preventDefault();
+      getInput();
+    });
+
+    $('.search-input').keypress(function() {
+      if(key == 13){
+        getInput();
+      }
+    });
+
+    function getInput(){
+      var searchAddress = $('input[name=address]').val();
+      console.log("success: " + searchAddress)
+      geocoder = new google.maps.Geocoder();
+      geocoder.geocode( { 'address': searchAddress }, function(results, status) {
+        console.log("success: " + results[0])
+        if (status == google.maps.GeocoderStatus.OK) {
+          searchInput = results[0].geometry.location;
+          map.setCenter(searchInput);
+        } else {
+          console.log("cannot geocode this string")
+        }
+      })
+    }
+
 
 
 
