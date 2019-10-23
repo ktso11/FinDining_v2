c26ab51feebb3d0eb948d2842503655cf58362f3
Katie So <katieso86@gmail.com>
2019-10-23 16:53:42 -0700

    pushing to heroku

--- routes/user.route.js
+++
@@ -1,8 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-//
-// const user_controller = require('../controller/user.controller');
-//
-// router.get('/test', user_controller.test);
-//
-// module.export = router;
